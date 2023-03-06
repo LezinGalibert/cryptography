2a962a471278f243370bc591b3bfeb54fb8f38dd
Lezin Galibert <lezin.galibert@softwarelab.no>
2023-03-06 20:42:56 +0100

    Refactored to use sliders

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
 import { useCallback, useState } from 'react';
 import './App.css';
-import { isMillerPrime, randomPrimeNumberBySize } from './utils/math';
+import { randomPrimeNumberBySize } from './utils/math';
 
 function App() {
-  const [prime, setPrime] = useState(0);
-  const [precision, setPrecision] = useState(10);
-  const [hasConfirmed, setHasConfirmed] = useState(false);
-  const [isPrime, setIsPrime] = useState(false);
-
   const [lowBound, setLowBound] = useState(1);
   const [upBound, setUpBound] = useState(2);
   const [confirmedPrimePicker, setHasConfirmedPrimePicker] = useState(false);
   const [newPrime, setNewPrime] = useState(0);
-
-  const renderResult = useCallback(() => {
-    return hasConfirmed ? <h1>{isPrime ? `${prime} is prime` : `${prime} is not prime`}</h1> : null;
-  }, [prime, hasConfirmed]);
-
-  const onSubmit = useCallback(() => {
-    setIsPrime(isMillerPrime(prime, precision));
-    setHasConfirmed(true);
-  }, [prime, precision]);
 
   const onSubmitPrimePicker = useCallback(() => {
     const p = randomPrimeNumberBySize(lowBound, upBound, 10);
@@ -36,59 +22,38 @@ function App() {
     <div className="App">
       <form
         onSubmit={(e) => {
-          onSubmit();
-          e.preventDefault();
-        }}>
-        <h1>Use Miller test to check if your input is prime!</h1>
-        <div className="card">
-          <input
-            type="text"
-            name="Pick your number"
-            value={prime}
-            onChange={(e) => {
-              setHasConfirmed(false);
-              setPrime(+e.target.value);
-            }}
-          />
-          <input
-            type="text"
-            name="Set a precision"
-            value={precision}
-            onChange={(e) => {
-              setHasConfirmed(false);
-              setPrecision(+e.target.value);
-            }}
-          />
-        </div>
-        <button type="submit">Submit</button>
-        {renderResult()}
-      </form>
-
-      <form
-        onSubmit={(e) => {
           onSubmitPrimePicker();
           e.preventDefault();
         }}>
         <h1>Give me sizes and I'll give you a prime between those sizes</h1>
         <div className="card">
-          <input
-            type="text"
-            name="Pick a lower size"
-            value={lowBound}
-            onChange={(e) => {
-              setHasConfirmedPrimePicker(false);
-              setLowBound(+e.target.value);
-            }}
-          />
-          <input
-            type="text"
-            name="Pick a higher size"
-            value={upBound}
-            onChange={(e) => {
-              setHasConfirmedPrimePicker(false);
-              setUpBound(+e.target.value);
-            }}
-          />
+          <div className="horizontal">
+            <p>{`Lower size: ${lowBound}`}</p>
+            <input
+              type="range"
+              name="Pick a lower size"
+              min={1}
+              max={7}
+              value={lowBound}
+              onChange={(e) => {
+                setHasConfirmedPrimePicker(false);
+                setLowBound(Math.min(+e.target.value, upBound));
+              }}
+            />
+
+            <p>{`Upper size: ${upBound}`}</p>
+            <input
+              type="range"
+              name="Pick a higher size"
+              min={2}
+              max={7}
+              value={upBound}
+              onChange={(e) => {
+                setHasConfirmedPrimePicker(false);
+                setUpBound(Math.max(+e.target.value, lowBound));
+              }}
+            />
+          </div>
         </div>
         <button type="submit">Submit</button>
         {renderNewPrime()}
